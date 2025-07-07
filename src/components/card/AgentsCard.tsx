61ff652d1ce037e19950911b5bfc56efac5999d4
rain44556 <bristy44556@gmail.com>
2025-07-07 19:30:02 +0600

    Add framer motion in client and improve card interaction with hover and entry animations

--- src/components/card/AgentsCard.tsx
+++ src/components/card/AgentsCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Agent } from "@/lib/types";
 import React from "react";
 import { Button } from "@/components/ui/button";
@@ -11,6 +13,7 @@ import {
 } from "@/components/ui/card";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "../ui/badge";
+import MotionDiv from "../animation/MotionDiv";
 
 type Props = {
   agent: Agent;
@@ -25,37 +28,39 @@ const AgentsCard = ({ agent }: Props) => {
 
   return (
     <>
-      <Card className="w-full">
-        <CardHeader>
-          <Avatar>
-            <AvatarImage className="object-cover" src={agent.image} />
-          </Avatar>
-          <div>
-            <CardTitle className="text-xl">{agent.name}</CardTitle>
-            <p className="text-sm text-muted-foreground mb-3">
-              {agent.category}
-            </p>
-          </div>
+      <MotionDiv
+        whileHover={{ scale: 1.05 }}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 10, y: 10 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}>
+        <Card className="w-full hover:bg-gradient-to-r from-green-800">
+          <CardHeader>
+            <Avatar>
+              <AvatarImage className="object-cover" src={agent.image} />
+            </Avatar>
+            <div>
+              <CardTitle className="text-xl">{agent.name}</CardTitle>
+              <p className="text-sm text-muted-foreground mb-3">
+                {agent.category}
+              </p>
+            </div>
+            <CardDescription>{agent.description}</CardDescription>
+            <CardAction>
+              <Badge
+                className={`${statusColor[agent.status]} ml-auto text-white`}
+              >
+                {agent.status}
+              </Badge>
+            </CardAction>
+          </CardHeader>
 
-          <CardDescription>
-            {agent.description}
-          </CardDescription>
-
-          <CardAction>
-            <Badge
-              className={`${statusColor[agent.status]} ml-auto text-white`}
-            >
-              {agent.status}
-            </Badge>
-          </CardAction>
-        </CardHeader>
-
-        <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full">
-            {agent.pricingModel}
-          </Button>
-        </CardFooter>
-      </Card>
+          <CardFooter className="flex-col gap-2">
+            <Button type="submit" className="w-full">
+              {agent.pricingModel}
+            </Button>
+          </CardFooter>
+        </Card>
+      </MotionDiv>
     </>
   );
 };
